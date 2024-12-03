c05cb55f1c40441711993cee14f626ca3b2c68f1
mauro <mcontaalternativa@gmail.com>
2024-12-03 10:58:02 -0300

    G:Caso Impossivel

--- velha.cpp
+++ velha.cpp
@@ -15,6 +15,23 @@
 
 int VerificaVelha( int velha[3][3] )
 {
+	int qnt_x = 0, qnt_o = 0;
+
+	//Mapeia a velha e seus valores
+
+	for (int i = 0; i < 3; i++){
+		for (int j = 0; j < 3; j++){
+			if (velha[i][j] == 2)
+				qnt_o++;
+			if (velha[i][j] == 1)
+				qnt_x++;
+		}
+	}
+
+	//Avalia se o jogo é impossível
+
+	if (qnt_x < qnt_o || qnt_x-1 > qnt_o)
+		return -2; // Retorna -2, que caracteriza jogo impossível
 	return 0; /*!< retorna zero para teste */ 
 }
 

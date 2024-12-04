7c952a85559358a2f260e22bc776b8298687d53e
mauro <mcontaalternativa@gmail.com>
2024-12-04 00:37:21 -0300

    G: Dois Vencedores

--- velha.cpp
+++ velha.cpp
@@ -13,6 +13,7 @@
  */ 
 int VerificaVelha(int velha[3][3]) {
     int qnt_x = 0, qnt_o = 0;
+    int x_win = 0, o_win = 0;
     // Mapeia a velha e seus valores
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
@@ -36,9 +37,9 @@ int VerificaVelha(int velha[3][3]) {
         ((velha[0][0] == 1 && velha[1][1] == 1 && velha[2][2] == 1) //  Avalia Diagonal principal 
         || 
         (velha[0][2] == 1 && velha[1][1] == 1 && velha[2][0] == 1))) { //  Avalia Diagonal oposta
-            if (qnt_o >= qnt_x)  // vitória de X, é valido para menos O que X
+            if ((qnt_o >= qnt_x) && (qnt_x) ) // vitória de X, é valido para menos O que X
                 return -2;
-            return 1;
+            x_win = 1;
         } else if ((velha[i][0] == 2 && velha[i][1] == 2 && velha[i][2] == 2)
         ||
         (velha[0][i] == 2 && velha[1][i] == 2 && velha[2][i] == 2)
@@ -48,14 +49,22 @@ int VerificaVelha(int velha[3][3]) {
         (velha[0][2] == 2 && velha[1][1] == 2 && velha[2][0] == 2))) { // Avalia Diagonal oposta
             if (qnt_o != qnt_x)  // Vitória de O, é válido para O igual X
                 return -2;
-            return 2;
+            o_win = 1;
         }
     }
+
+    if (x_win == o_win){
+        if (x_win == 1) //  Verifica duplos vencedores
+            return -2;
+        else if (qnt_o+qnt_x==9) // Verifica empate
+            return 0;
+    } else if ( x_win != o_win){ // Verifica vencedor
+        if (x_win == 1)
+            return 1;
+        return 2;
+    }
 // Se não foi um jogo impossível e não houve vitória,
-// e não há espaços mais a serem jogados só é possível empate
 // Para não instanciar outro contador de nulidade, consideramos qnt_o+qnt_x == 9
-    if (qnt_o+qnt_x == 9)
-        return 0;
     return -1; /*!< retorna indefinido para teste */
 }
 
